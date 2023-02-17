64648e922ede283544e56e98bf04f831930b71c9
beaverDamDemo <zdenevi@gmail.com>
2023-02-17 11:33:37 +0100

    update, layout je zdaj ok

--- src/App.vue
+++ src/App.vue
@@ -20,53 +20,64 @@
           </div>
         </Menu>
       </Header>
-      <Layout>
-        <Sider hide-trigger :style="{ background: '#fff' }">
-          <Menu
-            active-name="1-2"
-            theme="light"
-            width="auto"
-            :open-names="['1']"
-          >
-            <Submenu name="1">
-              <template slot="title">
-                <Icon type="ios-navigate"></Icon>
-                <router-link to="/">Home</router-link>
-              </template>
-              <MenuItem name="1-1">Option 1</MenuItem>
-              <MenuItem name="1-2">Option 2</MenuItem>
-              <MenuItem name="1-3">Option 3</MenuItem>
-            </Submenu>
-            <Submenu name="2">
-              <template slot="title">
-                <Icon type="ios-keypad"></Icon>
-                <router-link to="/vue-acceleration">Acc</router-link>
-              </template>
-              <MenuItem name="2-1">Option 1</MenuItem>
-              <MenuItem name="2-2">Option 2</MenuItem>
-            </Submenu>
-            <Submenu name="3">
-              <template slot="title">
-                <Icon type="ios-analytics"></Icon>
-                <router-link to="/form-iview">Form</router-link>
-              </template>
-              <MenuItem name="3-1">Option 1</MenuItem>
-              <MenuItem name="3-2">Option 2</MenuItem>
-            </Submenu>
-          </Menu>
-        </Sider>
-
-        <Content :style="{ padding: '0 50px' }">
-          <Breadcrumb :style="{ margin: '20px 0' }">
-            <BreadcrumbItem>Home</BreadcrumbItem>
-            <BreadcrumbItem>Components</BreadcrumbItem>
-            <BreadcrumbItem>Layout</BreadcrumbItem>
-          </Breadcrumb>
-          <Card>
-            <div style="min-height: 200px">
-              <router-view />
-            </div>
-          </Card>
+      <Layout :style="{ padding: '0 50px' }">
+        <Breadcrumb :style="{ margin: '16px 0' }">
+          <BreadcrumbItem>Home</BreadcrumbItem>
+          <BreadcrumbItem>Components</BreadcrumbItem>
+          <BreadcrumbItem>Layout</BreadcrumbItem>
+        </Breadcrumb>
+        <Content
+          :style="{ padding: '24px 0', minHeight: '280px', background: '#fff' }"
+        >
+          <Layout>
+            <Sider hide-trigger :style="{ background: '#fff' }">
+              <Menu
+                active-name="1-2"
+                theme="light"
+                width="auto"
+                :open-names="['1']"
+              >
+                <Submenu name="1">
+                  <template slot="title">
+                    <Icon type="ios-navigate"></Icon>
+                    <router-link to="/">Home</router-link>
+                  </template>
+                  <MenuItem name="1-1">Option 1</MenuItem>
+                  <MenuItem name="1-2">Option 2</MenuItem>
+                  <MenuItem name="1-3">Option 3</MenuItem>
+                </Submenu>
+                <Submenu name="2">
+                  <template slot="title">
+                    <Icon type="ios-keypad"></Icon>
+                    <router-link to="/vue-acceleration">Acc</router-link>
+                  </template>
+                  <MenuItem name="2-1">Option 1</MenuItem>
+                  <MenuItem name="2-2">Option 2</MenuItem>
+                </Submenu>
+                <Submenu name="3">
+                  <template slot="title">
+                    <Icon type="ios-analytics"></Icon>
+                    <router-link to="/form-iview">Form</router-link>
+                  </template>
+                  <MenuItem name="3-1">Option 1</MenuItem>
+                  <MenuItem name="3-2">Option 2</MenuItem>
+                </Submenu>
+              </Menu>
+            </Sider>
+            <Content
+              :style="{
+                padding: '24px',
+                minHeight: '280px',
+                background: '#fff',
+              }"
+            >
+              <Card>
+                <div style="min-height: 200px">
+                  <router-view />
+                </div>
+              </Card>
+            </Content>
+          </Layout>
         </Content>
         <Footer class="layout-footer-center"
           >2011-2016 &copy; TalkingData</Footer
