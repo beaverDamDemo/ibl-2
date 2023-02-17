ab443e7eea9d1cd76de6a2d4a6deff704aa5a919
beaverDamDemo <zdenevi@gmail.com>
2023-02-17 11:21:24 +0100

    fixed small mistake

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,34 @@
 <template>
-  <div id="app">
+  <div id="app" class="layout">
     <Layout>
-      <Sider hide-trigger>
-        <Sider hide-trigger :style="{ background: '#fff' }">
-          <Menu
-            active-name="1-2"
-            theme="light"
-            width="auto"
-            :open-names="['1']"
-          >
-            <Submenu name="1">
-              <template slot="title">
-                <Icon type="ios-navigate"></Icon>
-                <router-link to="/">Home</router-link>
-              </template>
-              <MenuItem name="1-1">Option 1</MenuItem>
-              <MenuItem name="1-2">Option 2</MenuItem>
-              <MenuItem name="1-3">Option 3</MenuItem>
-            </Submenu>
-            <Submenu name="2">
-              <template slot="title">
-                <Icon type="ios-keypad"></Icon>
-                <router-link to="/vue-acceleration">Acc</router-link>
-              </template>
-              <MenuItem name="2-1">Option 1</MenuItem>
-              <MenuItem name="2-2">Option 2</MenuItem>
-            </Submenu>
-            <Submenu name="3">
-              <template slot="title">
-                <Icon type="ios-analytics"></Icon>
-                <router-link to="/form-iview">Form</router-link>
-              </template>
-              <MenuItem name="3-1">Option 1</MenuItem>
-              <MenuItem name="3-2">Option 2</MenuItem>
-            </Submenu>
-          </Menu>
-        </Sider>
+      <Sider hide-trigger :style="{ background: '#fff' }">
+        <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+          <Submenu name="1">
+            <template slot="title">
+              <Icon type="ios-navigate"></Icon>
+              <router-link to="/">Home</router-link>
+            </template>
+            <MenuItem name="1-1">Option 1</MenuItem>
+            <MenuItem name="1-2">Option 2</MenuItem>
+            <MenuItem name="1-3">Option 3</MenuItem>
+          </Submenu>
+          <Submenu name="2">
+            <template slot="title">
+              <Icon type="ios-keypad"></Icon>
+              <router-link to="/vue-acceleration">Acc</router-link>
+            </template>
+            <MenuItem name="2-1">Option 1</MenuItem>
+            <MenuItem name="2-2">Option 2</MenuItem>
+          </Submenu>
+          <Submenu name="3">
+            <template slot="title">
+              <Icon type="ios-analytics"></Icon>
+              <router-link to="/form-iview">Form</router-link>
+            </template>
+            <MenuItem name="3-1">Option 1</MenuItem>
+            <MenuItem name="3-2">Option 2</MenuItem>
+          </Submenu>
+        </Menu>
       </Sider>
       <Layout>
         <Header>
