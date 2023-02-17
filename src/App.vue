44d8cb391f84a8b528d68ac6aabcaed1e99827f1
beaverDamDemo <zdenevi@gmail.com>
2023-02-17 10:50:10 +0100

    popravljen logo

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,49 @@
         <router-link to="/form-iview">Form Iview</router-link>
       </Sider>
       <Layout>
-        <Header
-          ><nav>
+        <Header>
+          <Menu mode="horizontal" theme="dark" active-name="1">
+            <div class="layout-logo"></div>
+            <div class="layout-nav">
+              <MenuItem name="1">
+                <Icon type="ios-navigate"></Icon>
+                Item 1
+              </MenuItem>
+              <MenuItem name="2">
+                <Icon type="ios-keypad"></Icon>
+                Item 2
+              </MenuItem>
+              <MenuItem name="3">
+                <Icon type="ios-analytics"></Icon>
+                Item 3
+              </MenuItem>
+              <MenuItem name="4">
+                <Icon type="ios-paper"></Icon>
+                Item 4
+              </MenuItem>
+            </div>
+          </Menu>
+          <nav>
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link> |
             <router-link to="/vue-acceleration">Vue acceleration</router-link> |
             <router-link to="/form-iview">Form Iview</router-link>
           </nav></Header
         >
-        <Content>
+        <Content :style="{ padding: '0 50px' }">
           <router-view />
+          <Breadcrumb :style="{ margin: '20px 0' }">
+            <BreadcrumbItem>Home</BreadcrumbItem>
+            <BreadcrumbItem>Components</BreadcrumbItem>
+            <BreadcrumbItem>Layout</BreadcrumbItem>
+          </Breadcrumb>
+          <Card>
+            <div style="min-height: 200px">Content</div>
+          </Card>
         </Content>
-        <Footer>Footer</Footer>
+        <Footer class="layout-footer-center"
+          >2011-2016 &copy; TalkingData</Footer
+        >
       </Layout>
     </Layout>
   </div>
@@ -38,5 +69,33 @@
   padding: 30px;
   text-align: center;
   font-size: 16px;
+}
+</style>
+
+<style scoped>
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+}
+.layout-logo {
+  width: 100px;
+  height: 30px;
+  background: #5b6270;
+  border-radius: 3px;
+  float: left;
+  position: relative;
+  top: 15px;
+  left: 20px;
+}
+.layout-nav {
+  width: 420px;
+  margin: 0 auto;
+  margin-right: 20px;
+}
+.layout-footer-center {
+  text-align: center;
 }
 </style>
