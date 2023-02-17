71d060b3c19c970a949bd329375b03305b390376
beaverDamDemo <zdenevi@gmail.com>
2023-02-17 10:41:54 +0100

    malo boljse kot prej

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/vue-acceleration">Vue acceleration</router-link> |
-      <router-link to="/form-iview">Form Iview</router-link>
-    </nav>
-    <router-view />
+    <Layout>
+      <Sider hide-trigger>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/vue-acceleration">Vue acceleration</router-link> |
+        <router-link to="/form-iview">Form Iview</router-link>
+      </Sider>
+      <Layout>
+        <Header
+          ><nav>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link> |
+            <router-link to="/vue-acceleration">Vue acceleration</router-link> |
+            <router-link to="/form-iview">Form Iview</router-link>
+          </nav></Header
+        >
+        <Content>
+          <router-view />
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
   </div>
 </template>
 
@@ -22,17 +37,6 @@
   min-height: 100vh;
   padding: 30px;
   text-align: center;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-size: 1.25rem;
-}
-
-nav a.router-link-exact-active {
-  font-weight: bold;
+  font-size: 16px;
 }
 </style>
--- src/views/FormIview.vue
+++ src/views/FormIview.vue
@@ -1,40 +1,13 @@
 <template>
-  <section>
-    <Layout>
-      <Header>Header</Header>
-      <Content>Content</Content>
-      <Footer>Footer</Footer>
-    </Layout>
-
-    <Layout>
-      <Header>Header</Header>
-      <Layout>
-        <Sider hide-trigger>Sider</Sider>
-        <Content>Content</Content>
-      </Layout>
-      <Footer>Footer</Footer>
-    </Layout>
-
-    <Layout>
-      <Header>Header</Header>
-      <Layout>
-        <Content>Content</Content>
-        <Sider hide-trigger>Sider</Sider>
-      </Layout>
-      <Footer>Footer</Footer>
-    </Layout>
-
-    <Layout>
-      <Sider hide-trigger>Sider</Sider>
-      <Layout>
-        <Header>Header</Header>
-        <Content>Content</Content>
-        <Footer>Footer</Footer>
-      </Layout>
-    </Layout>
-  </section>
+  <section></section>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style>
+.pitt {
+  margin-bottom: 1rem;
+}
+</style>
