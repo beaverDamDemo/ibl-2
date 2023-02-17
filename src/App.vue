3051d35cb1d0a1c145f11f75ee68df64b6eed319
beaverDamDemo <zdenevi@gmail.com>
2023-02-17 11:23:49 +0100

    mucc better

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,61 @@
 <template>
   <div id="app" class="layout">
     <Layout>
-      <Sider hide-trigger :style="{ background: '#fff' }">
-        <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-          <Submenu name="1">
-            <template slot="title">
+      <Header>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <div class="layout-logo"></div>
+          <div class="layout-nav">
+            <MenuItem name="1">
               <Icon type="ios-navigate"></Icon>
               <router-link to="/">Home</router-link>
-            </template>
-            <MenuItem name="1-1">Option 1</MenuItem>
-            <MenuItem name="1-2">Option 2</MenuItem>
-            <MenuItem name="1-3">Option 3</MenuItem>
-          </Submenu>
-          <Submenu name="2">
-            <template slot="title">
-              <Icon type="ios-keypad"></Icon>
-              <router-link to="/vue-acceleration">Acc</router-link>
-            </template>
-            <MenuItem name="2-1">Option 1</MenuItem>
-            <MenuItem name="2-2">Option 2</MenuItem>
-          </Submenu>
-          <Submenu name="3">
-            <template slot="title">
+            </MenuItem>
+            <MenuItem name="2">
               <Icon type="ios-analytics"></Icon>
+              <router-link to="/vue-acceleration">Acc</router-link>
+            </MenuItem>
+            <MenuItem name="3">
+              <Icon type="ios-paper"></Icon>
               <router-link to="/form-iview">Form</router-link>
-            </template>
-            <MenuItem name="3-1">Option 1</MenuItem>
-            <MenuItem name="3-2">Option 2</MenuItem>
-          </Submenu>
+            </MenuItem>
+          </div>
         </Menu>
-      </Sider>
+      </Header>
       <Layout>
-        <Header>
-          <Menu mode="horizontal" theme="dark" active-name="1">
-            <div class="layout-logo"></div>
-            <div class="layout-nav">
-              <MenuItem name="1">
+        <Sider hide-trigger :style="{ background: '#fff' }">
+          <Menu
+            active-name="1-2"
+            theme="light"
+            width="auto"
+            :open-names="['1']"
+          >
+            <Submenu name="1">
+              <template slot="title">
                 <Icon type="ios-navigate"></Icon>
                 <router-link to="/">Home</router-link>
-              </MenuItem>
-              <MenuItem name="2">
-                <Icon type="ios-analytics"></Icon>
+              </template>
+              <MenuItem name="1-1">Option 1</MenuItem>
+              <MenuItem name="1-2">Option 2</MenuItem>
+              <MenuItem name="1-3">Option 3</MenuItem>
+            </Submenu>
+            <Submenu name="2">
+              <template slot="title">
+                <Icon type="ios-keypad"></Icon>
                 <router-link to="/vue-acceleration">Acc</router-link>
-              </MenuItem>
-              <MenuItem name="3">
-                <Icon type="ios-paper"></Icon>
+              </template>
+              <MenuItem name="2-1">Option 1</MenuItem>
+              <MenuItem name="2-2">Option 2</MenuItem>
+            </Submenu>
+            <Submenu name="3">
+              <template slot="title">
+                <Icon type="ios-analytics"></Icon>
                 <router-link to="/form-iview">Form</router-link>
-              </MenuItem>
-            </div>
+              </template>
+              <MenuItem name="3-1">Option 1</MenuItem>
+              <MenuItem name="3-2">Option 2</MenuItem>
+            </Submenu>
           </Menu>
-        </Header>
+        </Sider>
+
         <Content :style="{ padding: '0 50px' }">
           <Breadcrumb :style="{ margin: '20px 0' }">
             <BreadcrumbItem>Home</BreadcrumbItem>
