8e6f2cdb49678852b7137fe23e1b28a99116c3f9
beaverDamDemo <zdenevi@gmail.com>
2023-02-20 10:03:56 +0100

    wip

--- src/views/MyFormView.vue
+++ src/views/MyFormView.vue
@@ -1,5 +1,26 @@
 <template>
-    <h1>brusca</h1>
+    <section>
+        <h1>brusca</h1>
+        <Row>
+            <Col>
+            <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
+                <FormItem prop="user">
+                    <Input type="text" v-model="formInline.user" placeholder="Username">
+                    <Icon type="ios-person-outline" slot="prepend"></Icon>
+                    </Input>
+                </FormItem>
+                <FormItem prop="password">
+                    <Input type="password" v-model="formInline.password" placeholder="Password">
+                    <Icon type="ios-locked-outline" slot="prepend"></Icon>
+                    </Input>
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
+                </FormItem>
+            </Form>
+            </Col>
+        </Row>
+    </section>
 </template>
 
 <script>
@@ -7,11 +28,34 @@ export default {
     name: 'brusca',
     data () {
         return {
-
+            formInline: {
+                user: '',
+                password: ''
+            },
+            ruleInline: {
+                user: [
+                    { required: true, message: 'Please fill in the user name', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: 'Please fill in the password.', trigger: 'blur' },
+                    { type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur' }
+                ]
+            }
         }
     },
     mounted () {
         console.log('mounted')
+    },
+    methods: {
+        handleSubmit (name) {
+            this.$refs[name].validate((valid) => {
+                if (valid) {
+                    this.$Message.success('Success!');
+                } else {
+                    this.$Message.error('Fail!');
+                }
+            })
+        }
     }
 }
 </script>
